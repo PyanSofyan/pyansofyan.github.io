95bd2223940d0ba28957ef7d05fd93c76db5af34
Sofyan <pyansofyan@hotmail.com>
2023-07-03 15:41:47 +0700

    Updates

---
+++ precache-manifest.e379ecd4d2d5f0402bc10f7de904acd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735277049c561a7298d37c8b7aa0810c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f824f840844cadd82b",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9d78e9350fd5228d9c0d",
+    "url": "/static/css/main.714ca1e9.chunk.css"
+  },
+  {
+    "revision": "89f824f840844cadd82b",
+    "url": "/static/js/2.78235a19.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.78235a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d78e9350fd5228d9c0d",
+    "url": "/static/js/main.5af392b7.chunk.js"
+  },
+  {
+    "revision": "ba605c326cb6afd65b72",
+    "url": "/static/js/runtime-main.8026f938.js"
+  }
+]);
